74cd25e0a59fd3988ad3c558c0b9ba9ea82e4b81
ignacioaris <arisignacio90@gmail.com>
2025-02-10 12:11:15 -0600

    Update SortingAlgorithmsTest.java

--- SortingAlgorithmsTest.java
+++ SortingAlgorithmsTest.java
@@ -1,86 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class SortingAlgorithmsTest {
+class SortingAlgorithmTest {
+    private Sort<Integer> sort;
 
-    @Test
-    public void testInsertionSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.insertionSort(array);
-        assertArrayEquals(expected, array);
+    SortTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        IComparator<Integer> comparator = Integer::compare;
+        this.sort = new Sort(comparator);
     }
 
     @Test
-    public void testMergeSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.mergeSort(array);
-        assertArrayEquals(expected, array);
+    void testInsertionSort() {
+        Integer[] array = new Integer[]{5, 3, 8, 4, 2};
+        Integer[] expected = new Integer[]{2, 3, 4, 5, 8};
+        this.sort.insertionSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 
     @Test
-    public void testQuickSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.quickSort(array);
-        assertArrayEquals(expected, array);
+    void testMergeSort() {
+        Integer[] array = new Integer[]{9, 7, 5, 3, 1};
+        Integer[] expected = new Integer[]{1, 3, 5, 7, 9};
+        this.sort.mergeSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 
     @Test
-    public void testRadixSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.radixSort(array);
-        assertArrayEquals(expected, array);
+    void testQuickSort() {
+        Integer[] array = new Integer[]{10, 15, 5, 3, 7};
+        Integer[] expected = new Integer[]{3, 5, 7, 10, 15};
+        this.sort.quickSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 
     @Test
-    public void testBucketSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.bucketSort(array);
-        assertArrayEquals(expected, array);
+    void testRadixSort() {
+        int[] array = new int[]{170, 45, 75, 90, 802, 24, 2, 66};
+        int[] expected = new int[]{2, 24, 45, 66, 75, 90, 170, 802};
+        this.sort.radixSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 
     @Test
-    public void testHeapSort() {
-        Integer[] array = {5, 2, 9, 1, 5, 6};
-        Integer[] expected = {1, 2, 5, 5, 6, 9};
-        SortingAlgorithms.heapSort(array);
-        assertArrayEquals(expected, array);
-    }
-
-
-    @Test
-    public void testEmptyArray() {
-        Integer[] array = {};
-        Integer[] expected = {};
-        SortingAlgorithms.insertionSort(array);
-        assertArrayEquals(expected, array);
+    void testBucketSort() {
+        float[] array = new float[]{0.42F, 0.32F, 0.23F, 0.52F, 0.25F, 0.47F, 0.51F};
+        float[] expected = new float[]{0.23F, 0.25F, 0.32F, 0.42F, 0.47F, 0.51F, 0.52F};
+        this.sort.bucketSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 
     @Test
-    public void testSingleElementArray() {
-        Integer[] array = {42};
-        Integer[] expected = {42};
-        SortingAlgorithms.insertionSort(array);
-        assertArrayEquals(expected, array);
-    }
-
-    @Test
-    public void testAlreadySortedArray() {
-        Integer[] array = {1, 2, 3, 4, 5};
-        Integer[] expected = {1, 2, 3, 4, 5};
-        SortingAlgorithms.insertionSort(array);
-        assertArrayEquals(expected, array);
-    }
-
-    @Test
-    public void testReverseSortedArray() {
-        Integer[] array = {5, 4, 3, 2, 1};
-        Integer[] expected = {1, 2, 3, 4, 5};
-        SortingAlgorithms.insertionSort(array);
-        assertArrayEquals(expected, array);
+    void testShellSort() {
+        Integer[] array = new Integer[]{19, 8, 7, 3, 5};
+        Integer[] expected = new Integer[]{3, 5, 7, 8, 19};
+        this.sort.shellSort(array);
+        Assertions.assertArrayEquals(expected, array);
     }
 }
