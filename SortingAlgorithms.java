cb59992396a32f26a5176e437183a20e49bc642e
ignacioaris <arisignacio90@gmail.com>
2025-02-10 11:40:15 -0600

    Update SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -4,5 +4,187 @@ import java.util.List;
 import java.util.Random;
 
 public class SortingAlgorithms {
-  
+
+    // Insertion Sort
+    public static <T extends Comparable<T>> void insertionSort(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    // Merge Sort
+    public static <T extends Comparable<T>> void mergeSort(T[] array) {
+        if (array.length > 1) {
+            int mid = array.length / 2;
+            T[] left = Arrays.copyOfRange(array, 0, mid);
+            T[] right = Arrays.copyOfRange(array, mid, array.length);
+            mergeSort(left);
+            mergeSort(right);
+            merge(array, left, right);
+        }
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] array, T[] left, T[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i].compareTo(right[j]) <= 0) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    // Quick Sort
+    public static <T extends Comparable<T>> void quickSort(T[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int low, int high) {
+        T pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                T temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        T temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+    // Radix Sort (para enteros)
+    public static void radixSort(Integer[] array) {
+        int max = Arrays.stream(array).max(Integer::compare).get();
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(array, exp);
+        }
+    }
+
+    private static void countingSort(Integer[] array, int exp) {
+        int n = array.length;
+        Integer[] output = new Integer[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(array[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(array[i] / exp) % 10] - 1] = array[i];
+            count[(array[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, array, 0, n);
+    }
+
+    // Bucket Sort (para enteros)
+    public static void bucketSort(Integer[] array) {
+        int max = Arrays.stream(array).max(Integer::compare).get();
+        int min = Arrays.stream(array).min(Integer::compare).get();
+        int bucketCount = max - min + 1;
+        List<List<Integer>> buckets = new ArrayList<>(bucketCount);
+        for (int i = 0; i < bucketCount; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (int num : array) {
+            buckets.get(num - min).add(num);
+        }
+
+        int index = 0;
+        for (List<Integer> bucket : buckets) {
+            for (int num : bucket) {
+                array[index++] = num;
+            }
+        }
+    }
+
+    // Heap Sort (sort adicional)
+    public static <T extends Comparable<T>> void heapSort(T[] array) {
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            T temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    private static <T extends Comparable<T>> void heapify(T[] array, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left].compareTo(array[largest]) > 0) {
+            largest = left;
+        }
+        if (right < n && array[right].compareTo(array[largest]) > 0) {
+            largest = right;
+        }
+        if (largest != i) {
+            T swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+            heapify(array, n, largest);
+        }
+    }
+
+    // Bogo Sort
+    public static <T extends Comparable<T>> void bogoSort(T[] array) {
+        Random random = new Random();
+        while (!isSorted(array)) {
+            shuffle(array, random);
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean isSorted(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1].compareTo(array[i]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static <T> void shuffle(T[] array, Random random) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 }
